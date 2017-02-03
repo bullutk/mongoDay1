da959ea98f91a0f52f1f6905432caf5f5a82769a
Daniel <bullsbbqtn@gmail.com>
2017-02-03 16:24:24 -0500

    mongo framework set up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var mongoConnectUrl = "mongodb://localhost:27017/dcclass"
+// make a global for a place to stash our db connection
+var db;
+
+mongoClient.connect(mongoConnectUrl, (error, database)=>{
+	db = database;
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.collection('students').find({}).toArray((error, studentResults)=>{
+  	console.log(studentResults);
+  	res.render('index', { students: studentResults });
+
+  })
 });
 
 module.exports = router;
